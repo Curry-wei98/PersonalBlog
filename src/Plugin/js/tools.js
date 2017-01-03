486207f3770676f2de9ea1502f74710c12e6c98e
Guaifan <zousitanghulu@foxmail.com>
2017-01-03 15:18:32 +0800

    事件重复绑定bug修复，交互优化

--- src/Plugin/js/tools.js
+++ src/Plugin/js/tools.js
@@ -7,70 +7,100 @@ export default class Tools {
     }
 
     //todo 考虑如何使用toggle优化
-    removeClass(element, className) {
+    static removeClass(element, className) {
         let classList = element.classList;
         classList.remove(className);
     };
 
-    addClass(element, className) {
+    static addClass(element, className) {
         let classList = element.classList;
         if (!classList.contains(className))
             classList.add(className);
     };
 
+    static removeAndAddListener(event, element, fun) {
+        element.removeEventListener(event, fun,false);
+        element.addEventListener(event, fun, false);
+    }
+
+    static ajaxGet(idName,url){
+        var xmlHttp;
+        if (window.XMLHttpRequest) { // 兼容 IE7+, Firefox, Chrome, Opera, Safari
+            xmlHttp = new XMLHttpRequest();
+        }
+        else { // 兼容IE6, IE5
+            xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlHttp.onreadystatechange = function () {
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+                document.getElementById(idName).innerHTML = xmlHttp.responseText;
+            }
+        };
+        xmlHttp.open("GET", url, true);
+        xmlHttp.send();
+    };
+
     hover(element, className) {
         //bar标签的悬浮旋转工具函数
         var that = this;
-        element.onmouseover = function () {
-            if (!(that.middle.classList.contains("displayNone") || (that.middle.classList.contains("hide"))))
+        var changeClass = function () {
+            if (!(that.middle.classList.contains("displayNone") ||
+                that.middle.classList.contains("hide")))
                 that.middle.className = className;
         };
-        element.onmouseout = function () {
+        var turnBack = function () {
             if (!(that.middle.classList.contains("displayNone") || (that.middle.classList.contains("hide"))))
                 that.middle.className = "normal";
         };
+
+        Tools.removeAndAddListener("mouseover", element, changeClass);
+        Tools.removeAndAddListener("mouseout", element, turnBack);
     };
 
     changeClick(element, idName, url) {
         //bar标签点击修改load的工具函数
         var that = this;
-        element.onclick = function () {
-            if (!that.middle.classList.contains("displayNone")) {
-                document.getElementById(idName).classList.remove("displayNone");
-                that.removeClass(that.middle, "hide");
-                that.removeClass(that.middle, "displayNone");
 
-                that.addClass(that.middle, "hide");
-                that.middle.addEventListener("webkitAnimationEnd", function () {
-                    if (that.middle.classList.contains("hide")) {
-                        that.addClass(that.middle, "displayNone");
-                        var xmlHttp;
-                        if (window.XMLHttpRequest) { // 兼容 IE7+, Firefox, Chrome, Opera, Safari
-                            xmlHttp = new XMLHttpRequest();
-                        }
-                        else { // 兼容IE6, IE5
-                            xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
-                        }
-                        xmlHttp.onreadystatechange = function () {
-                            if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-                                document.getElementById(idName).innerHTML = xmlHttp.responseText;
-                            }
-                        };
-                        xmlHttp.open("GET", url, true);
-                        xmlHttp.send();
-                    }
-                });
+        var  animationEndFunction=function () {
+            if (that.middle.classList.contains("hide")) {
+                Tools.addClass(that.middle, "displayNone");
+                Tools.ajaxGet(idName,url);
             }
-        }
+        };
+
+        var clickFunction=function(){
+            document.getElementById(idName).classList.remove("displayNone");
+            document.getElementById(idName).classList.remove("hide");
+            Tools.removeClass(that.middle, "hide");
+            Tools.removeClass(that.middle, "displayNone");
+
+            Tools.addClass(that.middle, "hide");
+
+            Tools.removeAndAddListener("webkitAnimationEnd",that.middle,animationEndFunction);
+        };
+
+        Tools.removeAndAddListener("click",element,clickFunction);
+
     };
 
     homeClick(element) {
         var that = this;
         element.onclick = function () {
-            if (that.middle.classList.contains("displayNone")) {
-                that.middle.classList.remove("displayNone");
-                that.middle.classList.remove("hide");
-                document.getElementById("tags").classList.add("displayNone");
+            if (that.middle.classList.contains("hide")) {
+                var tags = document.getElementById("tags");
+                var animationEndFunction=function(){
+                    if (tags.classList.contains("hide")) {
+                        tags.classList.add("displayNone");
+                        that.middle.classList.remove("displayNone");
+                        that.middle.classList.remove("hide");
+                    }
+                };
+
+                Tools.addClass(tags, "hide");
+
+                Tools.removeAndAddListener("webkitAnimationEnd",tags,animationEndFunction);
+
+
             }
         }
     }
