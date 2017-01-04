f60243cbf44e297a2847e9afd75fc5c79b7a184d
Guaifan <zousitanghulu@foxmail.com>
2017-01-04 23:31:55 +0800

    把titles隐藏了起来，并且绑定了点击显示的动画，但是存在重复绑定的可能性

--- src/Plugin/js/index.js
+++ src/Plugin/js/index.js
@@ -18,3 +18,22 @@ tools.changeClick(notes, "tags", "Tags/notes.html");
 tools.changeClick(daily, "tags", "Tags/notes.html");
 
 
+var hexagon=document.getElementsByClassName('hexagon');
+var tags=document.getElementById("tags");
+tags.addEventListener("click",function(event){
+    var element=event.toElement;
+    var path=event.path;
+    for(var i in path){
+        if(path[i].className=="hexagon"){//事件捕获
+            for(var j in hexagon){
+                if(hexagon[j]==path[i]){
+                    var titles=document.getElementsByClassName("titles");
+                    for(var k=0;k<titles.length;k++){
+                        titles[k].style.display="none";
+                    }
+                    titles[j].style.display="block";
+                }
+            }
+        }
+    }
+});
