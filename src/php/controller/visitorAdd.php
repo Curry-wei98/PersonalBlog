d3ddcc4c6dc7bf2a7fe6e0ce8dfba03dc6029aa2
Guaifan <zousitanghulu@foxmail.com>
2017-01-06 21:56:51 +0800

    获取IP功能添加了时间

--- src/php/controller/visitorAdd.php
+++ src/php/controller/visitorAdd.php
@@ -1,13 +1,17 @@
 <?php
-    include_once "../common/conn.php";
-//    $sql="update visitor set num=num+1";
-//    $mysqli_stmt=$conn->prepare($sql);
-//
-//    $result=$mysqli_stmt->execute();
-//    if(!$result){
-//        echo "error:".$mysqli_stmt->error;
-//    }else{
-//        echo "success";
-//    }
-//
+include_once "../common/conn.php";
+
+$ip = $_SERVER["REMOTE_ADDR"];
+$sql = "insert into visitor (ip,time) VALUES (?,now()) ";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $ip);
+
+$result = $stmt->execute();
+if (!$result) {
+    echo "error:" . $stmt->error;
+} else {
+    echo "success";
+}
+
+
 ?>
